29fdf9ea6dd3ba6cd004a42670330989ff5478e0
Eva <shepherdlord1119@gmail.com>
2022-11-28 22:18:07 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <Header/> -->
+   <!-- <router-view /> -->
+   <FileUpload/>
+  </div>
+</template>
+
+<script>
+// import Header from './components/Header.vue';
+import FileUpload from './page/FileUpload.vue'
+
+export default {
+  components:{
+    // homePage,
+    // UploadPage,
+    // SignPage,
+    // FileReview,
+    // Header,
+    FileUpload
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import './assets/css/all.scss';
+</style>
