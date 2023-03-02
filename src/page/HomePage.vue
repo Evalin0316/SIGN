4de48a9017c856fd1d78ea4988625cf7c50c1526
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2023-03-02 16:29:38 +0800

    feat:調整搜尋ui

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -4,13 +4,22 @@
     <div class="container_home p-3">
     <div>
         <div class="search_file flex flex-row">
-            <div class="search_input w-2/5 m-4">
-                <input type="text" class="bg-white m-3 h-8 rounded-lg p-1"/>
+            <form>
+            <div class="search_input m-4 relative">
+                <div class="absolute pointer-events-none right-3.5 bottom-1.5 mr-5 border-r-4 border-l-yellow-600"><img src="../assets/images/icon_Close_n.png"/></div>
+                <div class="absolute pointer-events-none right-3.5 bottom-1.5"><img src="../assets/images/icon_search_n.png"/></div>
+                <input type="text" class="bg-white m-3 h-8 rounded-lg block w-full
+                p-4 pl-10 text-sm text-gray-900 border border-gray-300
+                focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
+                dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Search here..."/>
             </div>
+            </form>
             <div class="search_type bg-white flex w-3/5 m-4 rounded-lg">
                 <label class="m-3"><input type="checkbox"/>未完成</label>
                 <label class="m-3"><input type="checkbox"/>已完成</label>
                 <label class="m-3"><input type="checkbox"/>已取消</label>
+                <label>共筆</label>
             </div>
         </div>
         <div class="filter_type"></div>
