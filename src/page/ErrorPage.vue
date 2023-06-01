e081372dc8e1a106b2e6cdc982b2a828767a7591
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2023-06-01 17:22:49 +0800

    test deploy

--- src/page/ErrorPage.vue
+++ src/page/ErrorPage.vue
@@ -2,7 +2,7 @@
 <div class="bg-[#EFE3D4] h-screen flex justify-center items-center flex-row">
     <div>
         <div class="text-lg">404 Not Found</div>
-        <div class="text-lg">Oops！您要找的頁面不存在~~~</div>
+        <div class="text-lg">Oops！您要找的頁面不存在~~</div>
         <div class="text-sm">
             <router-link to="/" class="text-[#BE8E55]">
             回到首頁</router-link>
